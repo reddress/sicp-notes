897a4a7c2f4bba56167a5584cf4e01df72444b37
Heitor Chang <heitorchang@gmail.com>
2017-06-30 21:50:44 -0300

    redo ch 5

---
+++ 2017_06_30/computing-with-register-mach.scm
@@ -0,0 +1,8 @@
+;; p. 491
+
+;; p. 497 GCD machine's controller sequence
+
+;; p. 512 Sec 5.1.5 Instruction summary
+
+;; Using MIT-Scheme, press C-c C-c to return to REPL level 1
+;; C-d will exit Scheme
